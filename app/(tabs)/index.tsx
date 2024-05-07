3daa65fbcb427ec04558629718a7db09f51f156c
PaccyC <abayisengaaimepacifique@gmail.com>
2024-05-07 21:04:53 +0200

    added color and font-family configurations in tailwind.config.js

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,10 +3,11 @@ import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router'
+import tw from 'twrnc'
 export default function TabOneScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text >Aora!</Text>
+      <Text className=''>Aora!</Text>
       <StatusBar style='auto'/>
       <Link href="/profile" style={{color:'blue'}}>Go to profile</Link>
     </View>
